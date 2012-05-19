b4b0f02d4c7e257e23852107e3d1d39a41be8f47
Joe Kelley <jfkelley@stanford.edu>
2012-05-18 17:55:41 -0700

    Added server-side support for tracking which users are using the app

---
+++ FickaPetsServer/src/com/server/fickapets/User.java
@@ -0,0 +1,38 @@
+package com.server.fickapets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+
+public class User {
+	private static final String ENTITY_KIND = "user";
+	private static final String USER_ID_PROPERTY = "uid";
+	
+	public static void create(String id) {
+		Entity entity = new Entity(ENTITY_KIND);
+		entity.setProperty(USER_ID_PROPERTY, id);
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		datastore.put(entity);
+	}
+	
+	public static boolean exists(String id) {
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Query query = new Query(ENTITY_KIND).addFilter(USER_ID_PROPERTY, FilterOperator.EQUAL, id);
+		return datastore.prepare(query).asIterator().hasNext();
+	}
+	
+	public static List<String> filterNonexisting(List<String> ids) {
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Query query = new Query(ENTITY_KIND).addFilter(USER_ID_PROPERTY, FilterOperator.IN, ids);
+		List<String> found = new ArrayList<String>();
+		for (Entity entity : datastore.prepare(query).asIterable()) {
+			found.add((String) entity.getProperty(USER_ID_PROPERTY));
+		}
+		return found;
+	}
+}
